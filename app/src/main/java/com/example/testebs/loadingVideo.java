75690070b9490d403c4247914365bd7cfdd7cba2
Anton-Sav <fk_bayern@mail.ru>
2019-10-08 05:16:43 +0300

    4

---
+++ app/src/main/java/com/example/testebs/loadingVideo.java
@@ -0,0 +1,32 @@
+package com.example.testebs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.concurrent.TimeUnit;
+
+public class loadingVideo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_video);
+
+        Intent intent = new Intent(this, lastActivity.class);
+
+        ProgressBar pg = findViewById(R.id.progressBar);
+        pg.setVisibility(View.VISIBLE);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
