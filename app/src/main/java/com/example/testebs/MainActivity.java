65ede6312693e2502387a801aa4d3236d549931d
Anton-Sav <fk_bayern@mail.ru>
2019-10-08 04:29:43 +0300

    2

--- app/src/main/java/com/example/testebs/MainActivity.java
+++ app/src/main/java/com/example/testebs/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.testebs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String defaultLine = "Чтобы получить биометрический доступ:" + "\n" +
+            "- ваше лицо должно быть хорошо видно;" + "\n" +
+            "- в помещении должно быть светло и тихо;" + "\n" +
+            "- если вы регистрировались в очках - наденьте их, если без - снимите.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView notification = findViewById(R.id.textView3);
+        notification.setText(defaultLine);
+
+        Button btn = findViewById(R.id.buttonStart);
+        btn.setTextColor(Color.WHITE);
     }
 }
